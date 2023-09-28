9378617a6fecfc233b848fd3afc28afe493c20d8
Ankit Tomar <ankitjdec@gmail.com>
2023-09-28 19:43:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p
+        style={{
+          fontFamily: "cursive",
+          fontSize: "32px",
+          fontWeight: "500",
+          color: "#fff",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        Welcome
+      </p>
     </div>
   );
 }
